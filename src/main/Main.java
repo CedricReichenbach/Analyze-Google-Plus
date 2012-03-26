42cac7ed7adacc29076b58efc16a7b2f74d088ff
Cedric Reichenbach <didu@gmx.ch>
2012-03-26 18:12:55 +0200

    non-running toy example

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,12 +1,22 @@
 package main;
 
+import com.google.api.client.http.HttpTransport;
+import com.google.api.client.http.javanet.NetHttpTransport;
+import com.google.api.client.json.JsonFactory;
+import com.google.api.client.json.jackson.JacksonFactory;
+
 public class Main {
 
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		
+		toyExample();
+	}
+
+	private static void toyExample() {
+		HttpTransport httpTransport = new NetHttpTransport();
+		JsonFactory jsonFactory = new JacksonFactory();
 	}
 
 }
